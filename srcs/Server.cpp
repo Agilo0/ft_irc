5b828bc871cd2b9c652b0c12b678625636ed4bd5
Agathe Lounici <alounici@cbr8s6.42barcelona.com>
2025-11-26 18:49:49 +0100

    --

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,26 +1,7 @@
 
-#include "server.hpp"
+#include "../inc/Server.hpp"
 #include <iostream>
 #include <sys/socket.h>
 
 
 
-server::server()
-{
-
-}
-
-server::server(int port, char *pw) : _port(port), _pw(pw)
-{
-    init_serv();
-}
-
-server::~server()
-{
-
-}
-
-void server::init_serv()
-{
-    _fd = socket(AF_INET, SOCK_STREAM, 0);
-}
