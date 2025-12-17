b2de9bc4172212e4a7a62f4be32d17d970fde549
Agathe Lounici <alounici@cbr8s6.42barcelona.com>
2025-12-17 17:43:16 +0100

    Revert "listooo"
    This reverts commit d09b749b5b2c08286916e80924be99851abdd8fe.

---
+++ exception.hpp
@@ -0,0 +1,21 @@
+#include <exception>
+
+
+class IncorrectPort : public std::exception 
+{
+    public:
+        virtual const char *what() const throw()
+        {
+            return ("Invalid port: needs only digits.");
+        }
+};
+
+
+class IncorrectPortRange : public std::exception
+{
+    public:
+        virtual const char *what() const throw()
+        {
+            return ("Invalid port: has to be between 49152 and 65535.");
+        }
+};
